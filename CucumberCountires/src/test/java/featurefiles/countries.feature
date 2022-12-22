2787e322b3997b282b1c9addacc29eb2c05b8fcd
pallavisadhu <pallavisadhu@yahoo.com>
2022-12-22 10:46:28 -0600

    Modified CucumberCountries file

--- CucumberCountires/src/test/java/featurefiles/countries.feature
+++ CucumberCountires/src/test/java/featurefiles/countries.feature
@@ -1,6 +1,11 @@
 Feature: Countries Capital
 
 Scenario: Verify countries, capital and language
-Given the user has three arrays
+Given the user has countries array
+|China|Egypt|France|Germany|India|Japan|Kenya|Mexico|United Kingdom|United States|
+And the user has capitals array
+|Beijing|Cario|Paris|Berlin|New Delhi|Tokyo|Nairobi|Mexico City|London|Washington D.C.|
+And the user has languages array
+|Chinese|Arabic|French|German|Hindi|Japanese|Swahili|Spanish|English|English|
 When the user choose a random number 
 Then the corresponding items should be printed
--- CucumberCountires/src/test/java/stepdef/CountrySelection.java
+++ CucumberCountires/src/test/java/stepdef/CountrySelection.java
@@ -5,43 +5,49 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import cucumber.api.DataTable;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class CountrySelection {
 	
-	String[] country= {"China", "Egypt", "France", "Germany", "India", "Japan", "Kenya", "Mexico", "United Kingdom", "United States"};
-	String[] capital= {"Beijing", "Cairo", "Paris", "Berlin", "New Delhi", "Tokyo", "Nairobi", "Mexico City", "London", "Washington D.C."};
-	String[] language= { "Chinese", "Arabic", "French", "German", "Hindi", "Japanese", "Swahili", "Spanish", "English", "English"};
-	List<String> countryList;
-	List<String> capitalList;
-	List<String> languageList;
+	List<String> countryList, capitalList, languageList;
 	int index;
 	
-	@Given("^the user has three arrays$")
-	public void the_user_has_three_arrays() throws Throwable {
-		countryList = new ArrayList<String>(Arrays.asList(country));
-	    capitalList = new ArrayList<String>(Arrays.asList(capital));
-	    languageList = new ArrayList<String>(Arrays.asList(language));
+	@Given("^the user has countries array$")
+	public void the_user_has_countries_array(DataTable arg1) throws Throwable {
+		
+		countryList = arg1.asList(String.class);
+	}
+
+	@And("^the user has capitals array$")
+	public void the_user_has_capitals_array(DataTable arg1) throws Throwable {
+		
+	    capitalList = arg1.asList(String.class);
+	}
+
+	@And("^the user has languages array$")
+	public void the_user_has_languages_array(DataTable arg1) throws Throwable {
+	   
+		languageList = arg1.asList(String.class);
 	}
 
 	@When("^the user choose a random number$")
 	public void the_user_choose_a_random_number() throws Throwable {
+		
 		int len = countryList.size();
 		Random r = new Random();
 		index = r.nextInt(len);
-	   
+		
 	}
 
 	@Then("^the corresponding items should be printed$")
 	public void the_corresponding_items_should_be_printed() throws Throwable {
-		System.out.println("Index:"+index);
-		System.out.println("Country:"+countryList.get(index));
-		System.out.println("Capital:"+capitalList.get(index));
-		System.out.println("Language:"+languageList.get(index));
+		System.out.println("Index:" + index);
+		System.out.println("Country:"+ countryList.get(index));
+		System.out.println("Capital:"+ capitalList.get(index));
+		System.out.println("Language:"+ languageList.get(index));
 	}
-
-
-
 }
